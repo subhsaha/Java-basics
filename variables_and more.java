1443583c2f23ea7fca94bbdcbc9386a79341d151
subhsaha <31180754+subhsaha@users.noreply.github.com>
2017-09-02 03:10:13 +0530

    Create variables_and more.java

---
+++ variables_and more.java
@@ -0,0 +1,24 @@
+
+public class apple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double tuna;
+		tuna = 5.28;
+
+		System.out.print("Hey this is double" + tuna);
+		System.out.println(tuna);
+    System.out.println(tuna + "  " + "Printed in next line")
+
+	}
+
+}
+// Explain Points:
+// Above program tells following thing:
+// >> syste.out.println will take the cursor to next line
+// >> where as system.out.print keeps it in the same line
+// >> public static void main(String[] args) is creating a method called main
+// everything runs under main,
+// without method name main program will not run and the mentioned statement is method
+// header and whatever written inside it is method body.
