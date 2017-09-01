f368bcc475e0417988c2dcdfeabe4cff78b39d24
subhsaha <31180754+subhsaha@users.noreply.github.com>
2017-09-02 04:06:14 +0530

    Create scanner.java

---
+++ scanner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class orange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner tuna = new Scanner(System.in);
+     
+     System.out.println(tuna.nextLine());
+     tuna.close();
+	}
+
+}
+//NOTES:
+//obj:  To take input from user.
+// import scanner package line 1
+
+// tuna is the variable, scanner tuna in line 6 says that use scanner with variable tuna
+// after assignment operator 2n part (system.in) take input from keyboard
+// Then printing what is stored in variable 
+//(tuna.nextLine()); wait  till it gets info from line no.6 and then print the output
+//tuna.close() programe will run without it but it is suggested to close the variable to avoid unnecessary use of space.
