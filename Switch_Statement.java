c788f22f1a9c2d14f879d7c4785cc3c497590daa
subhsaha <31180754+subhsaha@users.noreply.github.com>
2017-09-03 22:10:55 +0530

    Create Switch_Statement.java

---
+++ Switch_Statement.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Switch_Statement {
+	public static void main(String[] args) {
+		String one;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter Your Gender(M/F).");
+		one = input.nextLine();
+		switch (one) {
+		case "M":
+			System.out.println("Gender = Male");
+			break;
+		case "F":
+			System.out.println("Gender = Female");
+		default:
+			System.out.println("Not Disclosed");
+		}
+
+	}
+
+}
+
+//>> string input is taken from the user 
+//>> and used the input to display gender info 
+//>> switch statement is used 
+//>> variable 'one' is ued to take inputs and then passed into switch parameters 
+//>> and then case statement is used to verify the input and display relvent output
+//** Practice Problem**
